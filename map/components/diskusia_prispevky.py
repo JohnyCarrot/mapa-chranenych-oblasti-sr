0d598aa12e48125772ae1bc60b30e67312b58940
Roman Božik <web.kone@gmail.com>
2023-12-28 17:02:06 +0100

    diskusia...

--- map/components/diskusia_prispevky.py
+++ map/components/diskusia_prispevky.py
@@ -1,5 +1,18 @@
-from django_unicorn.components import UnicornView
+import json
 
+from django_unicorn.components import UnicornView
+from map.models import Diskusia,Diskusny_prispevok
 
 class DiskusiaPrispevkyView(UnicornView):
-    pass
+    prispevky = []
+    id_diskusie = ""
+    def mount(self):
+        if "q" in self.request.GET:
+            self.id_diskusie = self.request.GET.get("q")
+
+        if self.id_diskusie !="":
+            diskusia = Diskusia.objects.get(id=self.id_diskusie)
+            for prispevok in Diskusny_prispevok.objects.filter(diskusia=diskusia):
+                self.prispevky.append(prispevok)
+
+
