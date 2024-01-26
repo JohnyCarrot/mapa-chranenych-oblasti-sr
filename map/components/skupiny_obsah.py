50ac954b09e7870638f5f495074543a0316f7c6d
Roman Božik <web.kone@gmail.com>
2024-01-26 15:38:31 +0100

    skupiny zalozka

--- map/components/skupiny_obsah.py
+++ map/components/skupiny_obsah.py
@@ -2,18 +2,39 @@ from django_unicorn.components import UnicornView
 from map.views import over_viditelnost
 from map.models import Skupiny
 import json
+from difflib import SequenceMatcher
 class SkupinyObsahView(UnicornView):
     stranka_skupiny = True
     stranka_najst_skupinu = False
     vytvorit_skupinu = False #stranka
     skupiny = [] # skupiny v ktorých je užívatel
+    vyhladavacie_pole = ""
+    hladane_skupiny = []
     def mount(self):
         self.nahraj_skupiny()
 
+    def hladaj_skupinu(self):
+        self.hladane_skupiny.clear()
+        for skupina in Skupiny.objects.filter(spravca__isnull=False):
+            if skupina.nastavenia is not None and ("shared" in skupina.nastavenia or "own" in skupina.nastavenia):
+                continue
+            if over_viditelnost(skupina.viditelnost, self.request.user.is_authenticated,
+                                self.request.user.username) == False and self.request.user.username != skupina.spravca: continue
+            if self.request.user.username in skupina.viditelnost.uzivatelia or skupina.spravca ==self.request.user.username:
+                v_skupine = True
+            else:
+                v_skupine = False
+            meno = skupina.meno
+            if meno.lower().find(self.vyhladavacie_pole.lower()) != -1:
+                self.hladane_skupiny.append( (skupina,v_skupine)  )
+            elif self.similar(meno,self.vyhladavacie_pole) > 0.6:
+                self.hladane_skupiny.append( (skupina,v_skupine)  )
+        if len(self.hladane_skupiny)==0:
+            self.hladane_skupiny.append( (None,None) )
     def nahraj_skupiny(self):
         self.skupiny.clear()
         for skupina in Skupiny.objects.filter(spravca__isnull=False):
-            if over_viditelnost(skupina.viditelnost,self.request.user.is_authenticated,self.request.user.username) or skupina.spravca == self.request.user.username:
+            if self.request.user.username in skupina.viditelnost.uzivatelia or skupina.spravca == self.request.user.username:
                 if skupina.nastavenia is not None and ("shared" in skupina.nastavenia or "own" in skupina.nastavenia):
                     continue
 
@@ -27,13 +48,29 @@ class SkupinyObsahView(UnicornView):
         self.stranka_skupiny = True
         self.stranka_najst_skupinu = False
         self.vytvorit_skupinu = False
+        self.vyhladavacie_pole = ""
+        self.hladane_skupiny = []
 
     def prepni_na_najst_skupinu(self):
         self.stranka_skupiny = False
         self.stranka_najst_skupinu = True
         self.vytvorit_skupinu = False
+        self.vyhladavacie_pole = ""
+        self.hladane_skupiny = []
 
     def prepni_na_vytvorit_skupinu(self):
         self.stranka_skupiny = False
         self.stranka_najst_skupinu = False
         self.vytvorit_skupinu = True
+        self.vyhladavacie_pole = ""
+        self.hladane_skupiny = []
+
+    def pridat_sa_do_skupiny(self,id_skupiny):
+        skupina = Skupiny.objects.get(id = id_skupiny)
+        skupina.viditelnost.uzivatelia[self.request.user.username] = "r"
+        skupina.viditelnost.save()
+        skupina.save()
+
+
+    def similar(self,a, b):
+        return SequenceMatcher(None, a, b).ratio()
